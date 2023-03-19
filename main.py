0368dcaa3ecc4047dde14e6ab21dfbd7403c5965
Kartik Arcot <askarthikkumar@gmail.com>
2023-03-18 17:01:37 -0700

    description

--- main.py
+++ main.py
@@ -90,12 +90,15 @@ def create_app():
     # inputs is a list of a file input option and a dropdown option showing the already
     # existing files in the directory. The dropdown box is named "Choose From Existing Files"
     # and the file input box is named "Upload New File". inputs also has a box for input query.
+    description = "Talk to My Books is a tool that allows you to ask questions about a book and get answers." \
+                    " Built by <a href='https://twitter.com/kartikarcot'> Kartik Arcot </a> and <a href='https://twitter.com/HSlifelearner'> Harsh Sharma </a>." \
+                    " Powered by openai and llama-index!"
     inputs = [gr.File(),
             gr.Dropdown(list(get_existing_books(DATA_PATH).keys()),
                         label="Choose From Existing Files"),
             gr.Textbox(label="Query")]
     outputs = gr.HTML()
-    app = gr.Interface(analyze_file, inputs, outputs, title="Talk To My Books")
+    app = gr.Interface(analyze_file, inputs, outputs, title="Talk To My Books", description = description)
     return app
 
 if __name__ == "__main__":
